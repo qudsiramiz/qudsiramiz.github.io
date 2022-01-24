cde85b908f0f00216e4c27fa8e39f276013935e8
Ramiz Qudsi <qudsiramiz@gmail.com>
2022-01-24 13:56:25 -0500

    modified:   images/moving_pictures/DSCOVR_30days_hourly_averaged.mp4 	new file:   python/codes/__pycache__/dxl_dwld_upld_dscovr_30days_automated.cpython-39.pyc 	new file:   python/codes/__pycache__/dxl_mp4_dscovr.cpython-39.pyc 	new file:   python/codes/dscovr_30days_all.py 	new file:   python/codes/dxl_dwld_upld_dscovr_30days_automated.py 	new file:   python/codes/dxl_mp4_dscovr.py

---
+++ python/codes/dscovr_30days_all.py
@@ -0,0 +1,13 @@
+import os
+import time
+
+from dxl_dwld_upld_dscovr_30days_automated import plot_figures_dsco_30days
+from dxl_mp4_dscovr import make_gifs
+time_code_start = time.time()
+number_of_days = 365
+plot_figures_dsco_30days(number_of_days=number_of_days)
+make_gifs(number_of_days=number_of_days)
+
+# Copy the gif file to google drive
+os.system("cp /home/cephadrius/Desktop/git/qudsiramiz.github.io/images/moving_pictures/* /home/cephadrius/google-drive/Studies/Research/bu_research/dxl/figures/vids/")
+print(f"Time taken: {round(time.time() - time_code_start, 2)} seconds")
